7541274e6659e46a3d16e5829e6f71df3788c809
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2023-02-16 22:14:25 +0300

    fixed linter bugs-3

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,7 @@
-import { Request } from "express";
+import { Request } from 'express';
 
 export interface IRequest extends Request {
   user: {
     _id: string;
   };
 }
-
-// export type IRequest = Request<
-// ParamsDictionary, any, any, QueryString.ParsedQs, Record<string, any>
-// > & {
-//   user: {
-//     _id: string;
-//   };
-// }
